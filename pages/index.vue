85e9849a049a24f0f0cf4264bbfd1c144bb8cb8d
clpowe <clpowe@gmail.com>
2023-12-10 19:06:27 -0500

    Home page initial content

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,40 @@
 <template>
-	<div text="bold 2xl" font="sans">Home</div>
+	<main>
+		<h1>Revealing Truth Ministies</h1>
+		<article>
+			<h2>Recent Sermon</h2>
+			<iframe
+				width="560"
+				height="315"
+				src="https://www.youtube.com/embed/mXtx0s7dQ-s?si=aWgHj7o5zPBd02fp"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowfullscreen
+			></iframe>
+		</article>
+		<article>
+			<h2>Locations</h2>
+			<div>
+				<h3>Tampa</h3>
+				<p>Address</p>
+				<h4>Times</h4>
+				<p>Sunday</p>
+			</div>
+			<div>
+				<h3>St Pete</h3>
+				<p>Address</p>
+				<h4>Times</h4>
+				<p>Sunday</p>
+			</div>
+			<div>
+				<h3>Wesley Chaple</h3>
+				<p>Address</p>
+				<h4>Times</h4>
+				<p>Sunday</p>
+			</div>
+		</article>
+	</main>
 </template>
 
 <script setup lang="ts"></script>
