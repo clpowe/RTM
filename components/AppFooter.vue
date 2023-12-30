51a52bcc99203e46a076691ebfe3baf5fd81c356
clpowe <clpowe@gmail.com>
2023-12-29 22:22:27 -0500

    footer adjustments

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -6,12 +6,12 @@
 
 <template>
 	<footer class="content-grid bg-black p-4" text="white">
-		<div flex items-center justify-between>
-			<div flex items="center" gap-2>
+		<div flex flex-col items-center justify-between>
+			<div flex flex-col sm:flex-row items="center" gap-2>
 				<LogoLight text-4xl mb-0 />
-				<div>Revealing Truth Ministries - @ {{ date.getFullYear() }}</div>
+				<div>Revealing Truth Ministries {{ date.getFullYear() }}</div>
 			</div>
-			<div flex>
+			<div flex text-3xl gap-2>
 				<div class="i-bxl:facebook-square" text-white></div>
 				<div class="i-bxl:instagram-alt"></div>
 			</div>
