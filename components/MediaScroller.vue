1c0ffcbdc33be6137012443961a317cd8e4190b3
clpowe <clpowe@gmail.com>
2024-01-01 21:49:50 -0500

    added location scroll

--- components/MediaScroller.vue
+++ components/MediaScroller.vue
@@ -1,7 +1,45 @@
-<script setup></script>
+<script setup lang="ts">
+	const el = ref<HTMLElement | null>(null)
+	let isDown = ref(false)
+	let startX: number
+	let scrollLeft: number
+
+	function handleDown(e: any) {
+		if (el.value) {
+			isDown.value = true
+			startX = e.pageX - el.value?.offsetLeft
+			scrollLeft = el.value?.scrollLeft
+		}
+	}
+
+	function handleLeave(e: any) {
+		isDown.value = false
+	}
+
+	function handleUp(e: any) {
+		isDown.value = false
+	}
+
+	function handleMove(e: any) {
+		if (!isDown.value) return
+		if (el.value) {
+			const x = e.pageX - el.value.offsetLeft
+			const walk = (x - startX) * 3
+			el.value.scrollLeft = scrollLeft - walk
+		}
+	}
+</script>
 
 <template>
-	<div class="media-scroller">
+	<div
+		class="media-scroller"
+		@mousedown="handleDown"
+		@mouseleave="handleLeave"
+		@mouseup="handleUp"
+		@mousemove.prevent="handleMove"
+		:class="{ active: isDown }"
+		ref="el"
+	>
 		<slot />
 	</div>
 </template>
@@ -13,6 +51,7 @@
 
 	.media-scroller {
 		display: grid;
+		cursor: pointer;
 		grid-auto-flow: column;
 		grid-auto-columns: 90%;
 		gap: var(--size-3);
@@ -36,5 +75,9 @@
 		.media-scroller {
 			grid-auto-columns: 30%;
 		}
+	}
+
+	.active {
+		cursor: grabbing;
 	}
 </style>
